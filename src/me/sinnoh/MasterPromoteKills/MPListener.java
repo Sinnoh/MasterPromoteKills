ff7b3c24d9ca0423397229585ed6680a4bfd4769
Sinnoh <onkel1996@web.de>
2012-12-26 19:42:35 +0100

    Fixed NullPointerException

--- src/me/sinnoh/MasterPromoteKills/MPListener.java
+++ src/me/sinnoh/MasterPromoteKills/MPListener.java
@@ -4,6 +4,8 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import me.sinnoh.MasterPromoteKills.sPlayer;
 
@@ -15,26 +17,41 @@ public class MPListener implements Listener
 	@EventHandler
 	public void onPlayerDeath(EntityDeathEvent event)
 	{
-		if(event.getEntity().getType().equals(EntityType.PLAYER) && event.getEntity().getKiller().getType().equals(EntityType.PLAYER))//PVP
+		EntityDamageEvent devent = event.getEntity().getLastDamageCause();
+		if(devent instanceof EntityDamageByEntityEvent)
 		{
-			Player killer = (Player) event.getEntity().getKiller();
-			Player victim = (Player) event.getEntity();
-			sPlayer skiller = plugin.getsPlayer(killer);
-			sPlayer svictim = plugin.getsPlayer(victim);
-			skiller.addKill();
-			svictim.addDeath();
+			if(event.getEntity().getType().equals(EntityType.PLAYER) && event.getEntity().getKiller().getType().equals(EntityType.PLAYER))//PVP
+			{
+				Player killer = (Player) event.getEntity().getKiller();
+				Player victim = (Player) event.getEntity();
+				sPlayer skiller = plugin.getsPlayer(killer);
+				sPlayer svictim = plugin.getsPlayer(victim);
+				if(!killer.hasPermission("MasterPromoteKills.bypass.kills") && !killer.hasPermission("MasterPromoteKills.bypass.*"))
+				{
+					skiller.addKill();
+				}
+				if(!victim.hasPermission("MasterPromote.bypass.deaths") && !victim.hasPermission("MasterPromoteKills.bypass.*"))
+				{
+					svictim.addDeath();
+				}
+				sUtil.checkPlayer(killer);
+				sUtil.checkPlayer(victim);
+			}
+			else if(event.getEntity().getKiller().getType().equals(EntityType.PLAYER) && plugin.getConfig().getBoolean("CountPVEKills"))//PVE Player killed mob
+			{
+				Player killer = (Player) event.getEntity().getKiller();
+				sPlayer skiller = plugin.getsPlayer(killer);
+				skiller.addKill();
+				sUtil.checkPlayer(killer);
+			}
 		}
-		else if(event.getEntity().getKiller().getType().equals(EntityType.PLAYER) && plugin.getConfig().getBoolean("CountPVEKills"))//PVE Player killed mob
-		{
-			Player killer = (Player) event.getEntity().getKiller();
-			sPlayer skiller = plugin.getsPlayer(killer);
-			skiller.addKill();
-		}
-		else if(event.getEntity().getType().equals(EntityType.PLAYER) && plugin.getConfig().getBoolean("CountPVEDeaths"))//PVE Mob killed Player
+		else if(event.getEntity().getType().equals(EntityType.PLAYER) && plugin.getConfig().getBoolean("CountPVEDeaths"))//PVE Mob/Enviroement killed Player
 		{
 			Player victim = (Player) event.getEntity();
 			sPlayer svictim = plugin.getsPlayer(victim);
 			svictim.addDeath();
+			sUtil.log(svictim.getName() + svictim.getDeaths());
+			sUtil.checkPlayer(victim);
 		}
 	}
 
